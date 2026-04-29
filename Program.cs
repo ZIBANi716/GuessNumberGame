18f327f2e486054833739439a3fa53772189842c
ZIBANi716 <eto52rion@gmail.com>
2026-04-29 19:22:03 +0300

    initial commit:add english translation

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+
+namespace Тренировочные_проекты
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int random = rnd.Next(1, 101);
+            int guess = 0;
+            int attempts = 1;
+
+            Console.WriteLine("Игра:угадайте загаданное число!");
+            
+
+
+            while (guess != random)
+            {
+                Console.WriteLine("Введите ваше число!");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+
+                if (guess > random)
+                {
+                    Console.WriteLine("Меньше!Попробуйте еще!");
+                }
+                if (guess < random)
+                {
+                    Console.WriteLine("Больше!Попробуйте еще!");
+
+                }
+
+                else if (guess == random)
+                {
+                    Console.WriteLine("Ура!Вы угадали число за " + attempts + " попыток!");
+                }
+                
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
